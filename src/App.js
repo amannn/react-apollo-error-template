6c14a30bb6520bb70ef1dec0394ca6de8863261f
Jan Amann <jan@amann.me>
2017-08-08 11:21:22 +0200

    Reproduce action symmetry bug

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import { gql, graphql } from 'react-apollo';
 
 class App extends Component {
   render() {
-    const { data: { loading, people } } = this.props;
+    const { data: { loading, people, refetch } } = this.props;
     return (
       <main>
         <header>
-          <h1>Apollo Client Error Template</h1>
-          <p>
-            This is a template that you can use to demonstrate an error in Apollo Client.
-            Edit the source code and watch your browser window reload with the changes.
-          </p>
-          <p>
-            The code which renders this component lives in <code>./src/App.js</code>.
-          </p>
-          <p>
-            The GraphQL schema is in <code>./src/graphql/schema</code>.
-            Currently the schema just serves a list of people with names and ids.
-          </p>
+          <button onClick={() => refetch()}>Reload</button>
         </header>
         {loading ? (
           <p>Loading…</p>
